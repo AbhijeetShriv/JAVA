d63fde2d8b162ede2ec542f6323da1605b3f7072
AbhijeetShriv <abhijeetshrivas1@outlook.com>
2026-03-06 22:48:32 +0530

    Add files via upload

---
+++ MAIN10/Main10.java
@@ -0,0 +1,9 @@
+public class Main10{
+    public static void main(String[] args) {
+        int ur = 21;
+
+        for(int k=0;ur >= k; k++){
+            System.out.println("LOOP RUNS : " + k);
+        }
+    }
+}
