37cb92a24734f2f7daa7c8be13f14c914bd9ee7e
AbhijeetShriv <abhijeetshrivas1@outlook.com>
2026-03-06 22:56:31 +0530

    Add files via upload

---
+++ MAIN11/Main11.java
@@ -0,0 +1,15 @@
+class Name{
+    
+
+    void ager(int age){
+        System.out.println("AGE IS : " + age);
+    }
+}
+public class Main11{
+    public static void main(String[] args) {
+        Name n = new Name();
+
+        n.ager(32);
+
+    }
+}
